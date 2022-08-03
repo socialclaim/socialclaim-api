93f8fa90cfa808730ab1dd7e3460d1e12c925080
Yann Guillon <yann@dockdev.com>
2022-08-03 13:20:55 +0200

    basic fetch

--- app.rb
+++ app.rb
@@ -1,5 +1,29 @@
 require 'sinatra'
+require 'faraday'
+require 'uri'
+
 
 get '/' do
-  "Your Sinatra app is not quite Dokku-fied!"
+  conn = Faraday.new(
+    url: ENV['VUE_APP_VENLY_LOGIN_URL'],
+    headers: {'Content-Type' => 'application/x-www-form-urlencoded'}
+  )
+
+  response = conn.post('/auth/realms/Arkane/protocol/openid-connect/token') do |req|
+    req.body = URI.encode_www_form({
+                                     grant_type: 'client_credentials',
+                                     client_id: ENV['VUE_APP_VENLY_CLIENT_ID'],
+                                     client_secret: ENV['VUE_APP_SECRET_ID']
+                                   })
+  end
+  token = JSON.parse(response.body)['access_token']
+  pp token
+  content_type :json
+  conn = Faraday.new(
+    url: ENV['VUE_APP_VENLY_API_URL'],
+    headers: {'Content-Type' => 'application/json', 'Authorization' => "Bearer #{token}" }
+  )
+  wallets_response = conn.get('/api/wallets')
+  content_type :json
+  wallets_response.body.to_json
 end
