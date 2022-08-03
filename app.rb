489d17baa72756febfceaba8eacae0e1e4fe4539
Yann Guillon <yann@dockdev.com>
2022-08-03 17:44:30 +0200

    add find wallet

--- app.rb
+++ app.rb
@@ -1,14 +1,13 @@
 require 'sinatra'
 require 'faraday'
 require 'uri'
-require "sinatra/cors"
 
-set :allow_origin, "https://socialclaim.notifier.sh http://localhost:3000"
-set :allow_methods, "GET,HEAD,POST"
-set :allow_headers, "content-type,if-modified-since"
-set :expose_headers, "location,link"
+# set :allow_origin, "https://socialclaim.notifier.sh http://localhost:3000"
+# set :allow_methods, "GET,HEAD,POST"
+# set :allow_headers, "content-type,if-modified-since"
+# set :expose_headers, "location,link"
 
-get '/' do
+get '/wallets' do
   conn = Faraday.new(
     url: ENV['VUE_APP_VENLY_LOGIN_URL'],
     headers: {'Content-Type' => 'application/x-www-form-urlencoded'}
@@ -31,4 +30,36 @@ get '/' do
   wallets_response = conn.get('/api/wallets')
   content_type :json
   wallets_response.body.to_json
+end
+
+get '/wallets/:id' do
+  conn = Faraday.new(
+    url: ENV['VUE_APP_VENLY_LOGIN_URL'],
+    headers: {'Content-Type' => 'application/x-www-form-urlencoded'}
+  )
+
+  response = conn.post('/auth/realms/Arkane/protocol/openid-connect/token') do |req|
+    req.body = URI.encode_www_form({
+                                     grant_type: 'client_credentials',
+                                     client_id: ENV['VUE_APP_VENLY_CLIENT_ID'],
+                                     client_secret: ENV['VUE_APP_SECRET_ID']
+                                   })
+  end
+  token = JSON.parse(response.body)['access_token']
+  pp token
+  content_type :json
+  conn = Faraday.new(
+    url: ENV['VUE_APP_VENLY_API_URL'],
+    headers: {'Content-Type' => 'application/json', 'Authorization' => "Bearer #{token}" }
+  )
+  wallets_response = conn.get("/api/wallets/#{params['id']}")
+  content_type :json
+  wallets_response.body.to_json
+end
+
+options "*" do
+  response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  200
 end
--- config.ru
+++ config.ru
@@ -1,2 +1,11 @@
 require './app'
+# require 'rack/cors'
+#
+# use Rack::Cors do
+#   allow do
+#     origins '*' # 'localhost:3000', 'localhost:8080', '0.0.0.0:3000', '127.0.0.1:3000',
+#     resource '*',  headers: :any, methods: [:get, :post, :delete, :put, :patch, :options, :head]
+#   end
+# end
+
 run Sinatra::Application
